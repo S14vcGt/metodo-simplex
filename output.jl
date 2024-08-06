3ea58b876b702fe40483a63b9befd08a7c3b18da
Sebastian Mata <sebasmata41@gmail.com>
2024-08-06 00:48:09 -0400

    bonito

--- output.jl
+++ output.jl
@@ -7,7 +7,7 @@ end
 function escribirTablaFinal(sol::Vector)
     printstyled("\n La tabla final es:\n"; color=:cyan, bold=true)
     for i in eachindex(sol)
-        printstyled("$(sol[i])\n"; color=:light_cyan)
+        printstyled("$(round.(sol[i],digits=4))\n"; color=:light_cyan)
     end
 end
 
@@ -17,7 +17,7 @@ end
 
 function escribirVector(tabla::Vector)
     for i in eachindex(tabla)
-        printstyled("$(tabla[i])\n"; color=:light_blue)
+        printstyled("$(round.(tabla[i],digits=4))\n"; color=:light_blue)
     end
     print("\n")
 end
